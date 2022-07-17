1f2035a65c87ae1a1ab7afcf47e33a4305dc47e7
SidhdhiRaul <109357363+SidhdhiRaul@users.noreply.github.com>
2022-07-17 16:38:11 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import {Fragment} from 'react';
 import Search from './component/search';
-import DropDown from './component/DropDown'
+import Department from './component/Department';
+import Location from './component/Location';
+import Function from './component/Function';
+import JobsOpening from './component/JobsOpening';
 
 function App() {
   return (
     <Fragment>
       <Search></Search>
-      <DropDown></DropDown>
+      <Department></Department>
+      <Location></Location>
+      <Function></Function>
+      <JobsOpening></JobsOpening>
     </Fragment>
   );
 }
